4252c48833a15b6b5fd311b038916974d4dffe47
BuildTools <unconfigured@null.spigotmc.org>
2021-05-02 22:48:04 +0200

    feat: Started on Socket with requests

--- socket.tsx
+++ socket.tsx
@@ -8,4 +8,13 @@ if (!has('socket')) {
 
 let socket = get('socket') as Socket;
 
+// TODO: Meant to be a request system: client sends message that ends with reqid and then server respond with msg that ends with reqid then client method returns
+let reqid = 0;
+function request(...args: any[]) {
+    reqid++;
+    return new Promise((res, rej) => {
+
+    });
+}
+
 export { socket }
